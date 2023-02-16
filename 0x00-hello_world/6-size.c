7af6018b1e831a94b86bfcbbdf6225f67745c453
TendaiAndrew <juniornyakumbi@gmail.com>
2023-02-16 11:49:00 -0800

    there is hope

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,11 @@
+vi 6-size.c
 #include <stdio.h>
+
 /**
- * main - A program that prints the size of various computer types
- * Return: 0 (Success)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 
