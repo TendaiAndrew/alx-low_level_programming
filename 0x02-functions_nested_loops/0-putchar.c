604f50fa73d16fff82c5d9197d18f79ca2b7d8ec
TendaiAndrew <juniornyakumbi@gmail.com>
2023-02-21 10:10:02 -0800

    this is fun

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
-
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
-
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
 	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
