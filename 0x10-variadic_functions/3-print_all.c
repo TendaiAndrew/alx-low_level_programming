910c5d6e40beb6aa6a690b39912b28aa31dd20c9
TendaiAndrew <juniornyakumbi@gmail.com>
2023-03-23 16:56:56 -0700

    variadic functions are cool

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,18 +1,18 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
- * print_all - prints anything
- * @format: list of types of arguments passed to the function
- */
+* print_all - prints anything
+* @format: list of types of arguments passed to the function
+*/
 void print_all(const char * const format, ...)
 {
 	int i = 0;
 	char *str, *sep = "";
-
 	va_list list;
 
 	va_start(list, format);
-
 	if (format)
 	{
 		while (format[i])
@@ -42,8 +42,6 @@ void print_all(const char * const format, ...)
 			i++;
 		}
 	}
-
 	printf("\n");
 	va_end(list);
 }
-
